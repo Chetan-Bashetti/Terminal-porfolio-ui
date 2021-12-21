6a2f21bf576b16b4be493b60e48657b12549e6b0
chetan-vc <chetan.bashiti@viitor.cloud>
2021-12-21 20:22:55 +0530

    Updated worked orginazarions

--- src/components/Experiance/index.js
+++ src/components/Experiance/index.js
@@ -1,33 +1,37 @@
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
 const Wrapper = styled.div`
-  flex: 1;
-  display: flex;
-  padding: 1em;
-  margin: 0.5em 0;
-  border: 1px solid;
-  width: fit-content;
-  flex-direction: column;
+	flex: 1;
+	display: flex;
+	padding: 1em;
+	margin: 0.5em 0;
+	border: 1px solid;
+	width: fit-content;
+	flex-direction: column;
 `;
 
 const Title = styled.div`
-  font-size: 1.5em;
-  margin-right: 0.5em;
+	font-size: 1.5em;
+	margin-right: 0.5em;
 `;
 
 const CourseDescription = styled.div`
-  line-height: 2;
+	line-height: 2;
 `;
 
-const Experiance = ({ experinace = {} }) => {
-  return (
-    <Wrapper>
-      <Title>{experinace.org}</Title>
-      <CourseDescription>
-        {experinace.desg}, {experinace.period}
-      </CourseDescription>
-    </Wrapper>
-  );
+const Experiance = ({ experinace = [] }) => {
+	return (
+		<>
+			{experinace?.map((eachExp, id) => (
+				<Wrapper key={id}>
+					<Title>{eachExp.org}</Title>
+					<CourseDescription>
+						{eachExp.desg}, {eachExp.period}
+					</CourseDescription>
+				</Wrapper>
+			))}
+		</>
+	);
 };
 export default Experiance;
--- src/store/index.js
+++ src/store/index.js
@@ -1,68 +1,72 @@
 const STORE = {
-  SKILLS: [
-    'HTML5',
-    'CSS3',
-    'Javascript',
-    'ReactJs',
-    'Redux',
-    'ReactNative',
-    'VueJs',
-    'SailsJs',
-    'NodeJs',
-    'JQuery',
-    'NextJs',
-    'React Ionic',
-    'Git',
-    'Github',
-    'Bitbucket',
-    'Jira',
-    'VSCode',
-    'Postman',
-  ],
-  EDUCATION: [
-    {
-      title: 'Diploma',
-      timeline: '2011-14',
-      description:
-        'Completed Diploma in B.L.D.E.A`s Ploytechnic Vijayapura, Karnataka.',
-    },
-    {
-      title: 'Engineering',
-      timeline: '2014-18',
-      description:
-        ' Completed Engineering in PES South Campus Bangalore, Karnataka.',
-    },
-  ],
-  CONTACT: [
-    {
-      medium: 'Email',
-      info: 'chetankb619@gmail.com',
-    },
-    {
-      medium: 'phone',
-      info: '+91 8495011619',
-    },
-  ],
-  ABOUT: {
-    profilePic: 'assets/chetan.jpeg',
-    name: 'Chetan Bashetti',
-    desg: 'Software Developer, ',
-    location: 'Bangalore, Karnataka',
-  },
-  EXPERIANCE: {
-    org: 'Rayabhari Technologies, Bangalore.',
-    desg: 'Software Developer',
-    period: ' August 2018 - PRESENT',
-  },
-  SOCIAL_LINKS: [
-    {
-      platform: 'LinkedIn',
-      link: 'https://www.linkedin.com/in/chetan-bashetti-651643193/',
-    },
-    {
-      platform: 'Github',
-      link: 'https://github.com/Chetan-Bashetti',
-    },
-  ],
-}
-export default STORE
+	SKILLS: [
+		'HTML5',
+		'CSS3',
+		'Javascript',
+		'ReactJs',
+		'Redux',
+		'ReactNative',
+		'VueJs',
+		'NextJs',
+		'React Ionic',
+		'Git',
+		'Github',
+		'Bitbucket',
+		'Jira',
+		'VSCode',
+		'Postman'
+	],
+	EDUCATION: [
+		{
+			title: 'Diploma',
+			timeline: '2011-14',
+			description:
+				'Completed Diploma in B.L.D.E.A`s Ploytechnic Vijayapura, Karnataka.'
+		},
+		{
+			title: 'Engineering',
+			timeline: '2014-18',
+			description:
+				' Completed Engineering in PES South Campus Bangalore, Karnataka.'
+		}
+	],
+	CONTACT: [
+		{
+			medium: 'Email',
+			info: 'chetankb619@gmail.com'
+		},
+		{
+			medium: 'phone',
+			info: '+91 8495011619'
+		}
+	],
+	ABOUT: {
+		profilePic: 'assets/chetan.jpeg',
+		name: 'Chetan Bashetti',
+		desg: 'Software Developer, ',
+		location: 'Bangalore, Karnataka'
+	},
+	EXPERIANCE: [
+		{
+			org: 'Rayabhari Technologies, Bangalore.',
+			desg: 'Software Developer',
+			period: 'August 2018 - December 2020'
+		},
+		{
+			org: 'Vittor Cloud Technologies, Ahmedabad',
+			desg: 'Software Developer',
+			period: 'Februry 2021 - Present'
+		}
+	],
+	SOCIAL_LINKS: [
+		{
+			platform: 'LinkedIn',
+			link: 'https://www.linkedin.com/in/chetan-bashetti-651643193/'
+		},
+		{
+			platform: 'Github',
+			link: 'https://github.com/Chetan-Bashetti'
+		}
+	]
+};
+export default STORE;
